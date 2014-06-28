cfe8b2fcb7e3010ab1a740a32deffbf495b06ffd
android <trevd1234@gmail.com>
2014-06-28 10:51:04 +0100

    Update libEGL tests makefile - Use framework libEGL / libGLESv2

--- libEGL/tests/Android.mk
+++ libEGL/tests/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 include $(ANDROID_BUILD_TOP)/hardware/broadcom/$(TARGET_BOARD_PLATFORM)/cflags.mk
 
 LOCAL_C_INCLUDES += hardware/broadcom/$(TARGET_BOARD_PLATFORM)/include
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)/egl
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 
 LOCAL_SRC_FILES := hello_triangle.c utils.c
 LOCAL_MODULE := hello_triangle
 LOCAL_MODULE_TAGS := tests
-LOCAL_SHARED_LIBRARIES := libEGL_bcm2708 libgralloc.bcm2708 libvchostif liblog
+LOCAL_SHARED_LIBRARIES := libEGL libGLESv2 libgralloc.bcm2708 libvchostif liblog
 
 include $(BUILD_EXECUTABLE)
