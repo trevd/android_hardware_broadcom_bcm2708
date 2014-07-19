caeb43f4e298f7d3b2053ee210e8b6e155ab4f01
android <trevd1234@gmail.com>
2014-07-19 02:28:30 +0100

    Change libEGL to libGLES

--- libEGL/Android.mk
+++ libEGL/Android.mk
@@ -11,7 +11,7 @@ include $(BUILD_MULTI_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_LDFLAGS := -Wl,--no-warn-shared-textrel
-LOCAL_MODULE := libEGL_bcm2708
+LOCAL_MODULE := libGLES_bcm2708
 LOCAL_SHARED_LIBRARIES := liblog libdl 
 LOCAL_WHOLE_STATIC_LIBRARIES := libkhrn_static
 LOCAL_MODULE_TAGS:= optional
